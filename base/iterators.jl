a893e30aa07cbaf226010b7957f44c14cf31d3a8
Michael Abbott <me@escbook>
2020-10-23 16:05:16 +0200

    fix module qualifiers

--- base/iterators.jl
+++ base/iterators.jl
@@ -272,7 +272,7 @@ setindex!(v::Pairs, value, key) = (v.data[key] = value; v)
 get(v::Pairs, key, default) = get(v.data, key, default)
 get(f::Base.Callable, v::Pairs, key) = get(f, v.data, key)
 
-Base.map(f, A::Iterators.Pairs) = collect(Generator(f,A))
+Base.map(f, A::Pairs) = collect(Base.Generator(f,A))
 
 # zip
 
